b71f39ff79a1dc9a23db7b438aed091630f5d951
Delbin George <64653923+delbingeorge@users.noreply.github.com>
2022-04-03 16:39:33 +0530

    Add files via upload

---
+++ script.js/script.js
@@ -0,0 +1,28 @@
+let audio = document.getElementById("audio");
+function playPause() {
+    return audio.paused ? audio.play() : audio.pause();
+}
+let audio_one = document.getElementById("audio_one");
+function playPauseOne() {
+    return audio_one.paused ? audio_one.play() : audio_one.pause();
+}
+let audio_two = document.getElementById("audio");
+function playPause() {
+    return audio.paused ? audio.play() : audio.pause();
+}
+let audio_three = document.getElementById("audio");
+function playPause() {
+    return audio.paused ? audio.play() : audio.pause();
+}
+let audio_four = document.getElementById("audio");
+function playPause() {
+    return audio.paused ? audio.play() : audio.pause();
+}
+let audio_five = document.getElementById("audio");
+function playPause() {
+    return audio.paused ? audio.play() : audio.pause();
+}
+let audio_six = document.getElementById("audio");
+function playPause() {
+    return audio.paused ? audio.play() : audio.pause();
+}
