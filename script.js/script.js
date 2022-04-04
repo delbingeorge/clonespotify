9c8e893b55ea43235f13294b5282f3a1288c6d99
Delbin George <64653923+delbingeorge@users.noreply.github.com>
2022-04-04 18:57:51 +0530

    Update script.js

--- script.js/script.js
+++ script.js/script.js
@@ -2,27 +2,33 @@ let audio = document.getElementById("audio");
 function playPause() {
     return audio.paused ? audio.play() : audio.pause();
 }
+
 let audio_one = document.getElementById("audio_one");
 function playPauseOne() {
     return audio_one.paused ? audio_one.play() : audio_one.pause();
 }
-let audio_two = document.getElementById("audio");
-function playPause() {
-    return audio.paused ? audio.play() : audio.pause();
+
+let audio_two = document.getElementById("audio_two");
+function playPauseTwo() {
+    return audio_two.paused ? audio_two.play() : audio_two.pause();
 }
-let audio_three = document.getElementById("audio");
-function playPause() {
-    return audio.paused ? audio.play() : audio.pause();
+
+let audio_three = document.getElementById("audio_three");
+function playPauseThree() {
+    return audio_three.paused ? audio_three.play() : audio_three.pause();
 }
-let audio_four = document.getElementById("audio");
-function playPause() {
-    return audio.paused ? audio.play() : audio.pause();
+
+let audio_four = document.getElementById("audio_four");
+function playPauseFour() {
+    return audio_four.paused ? audio_four.play() : audio_four.pause();
 }
-let audio_five = document.getElementById("audio");
-function playPause() {
-    return audio.paused ? audio.play() : audio.pause();
+
+let audio_five = document.getElementById("audio_five");
+function playPauseFive() {
+    return audio_five.paused ? audio_five.play() : audio_five.pause();
 }
-let audio_six = document.getElementById("audio");
-function playPause() {
-    return audio.paused ? audio.play() : audio.pause();
+
+let audio_six = document.getElementById("audio_six");
+function playPauseSix() {
+    return audio_six.paused ? audio_six.play() : audio_six.pause();
 }
