359dfbc7a8fdae69ec0ba19ec4cb156d2f488c44
Delbin George <64653923+delbingeorge@users.noreply.github.com>
2022-04-08 18:15:11 +0530

    Update script.js

--- script.js/script.js
+++ script.js/script.js
@@ -1,34 +1,44 @@
-let audio = document.getElementById("audio");
-function playPause() {
-    return audio.paused ? audio.play() : audio.pause();
+var audioOne = document.querySelector("#audio");
+var audioTwo = document.querySelector("#audio_one");
+var audioThree = document.querySelector("#audio_two");
+var audioFour = document.querySelector("#audio_three");
+var audioFive = document.querySelector("#audio_four");
+var audioSix = document.querySelector("#audio_five");
+var audioSeven = document.querySelector("#audio_six");
+
+var allAudios = document.querySelectorAll("audio");
+
+function stopAllAudio() {
+    allAudios.forEach(function (audio) {
+        audio.pause();
+    });
 }
 
-let audio_one = document.getElementById("audio_one");
-function playPauseOne() {
-    return audio_one.paused ? audio_one.play() : audio_one.pause();
-}
-
-let audio_two = document.getElementById("audio_two");
-function playPauseTwo() {
-    return audio_two.paused ? audio_two.play() : audio_two.pause();
-}
-
-let audio_three = document.getElementById("audio_three");
-function playPauseThree() {
-    return audio_three.paused ? audio_three.play() : audio_three.pause();
-}
-
-let audio_four = document.getElementById("audio_four");
-function playPauseFour() {
-    return audio_four.paused ? audio_four.play() : audio_four.pause();
-}
-
-let audio_five = document.getElementById("audio_five");
-function playPauseFive() {
-    return audio_five.paused ? audio_five.play() : audio_five.pause();
-}
-
-let audio_six = document.getElementById("audio_six");
-function playPauseSix() {
-    return audio_six.paused ? audio_six.play() : audio_six.pause();
-}
+document.querySelector("#play-1").addEventListener("click", function () {
+    stopAllAudio();
+    audioOne.play();
+});
+document.querySelector("#play-2").addEventListener("click", function () {
+    stopAllAudio();
+    audioTwo.play();
+});
+document.querySelector("#play-3").addEventListener("click", function () {
+    stopAllAudio();
+    audioThree.play();
+});
+document.querySelector("#play-4").addEventListener("click", function () {
+    stopAllAudio();
+    audioFour.play();
+});
+document.querySelector("#play-5").addEventListener("click", function () {
+    stopAllAudio();
+    audioFive.play();
+});
+document.querySelector("#play-6").addEventListener("click", function () {
+    stopAllAudio();
+    audioSix.play();
+});
+document.querySelector("#play-7").addEventListener("click", function () {
+    stopAllAudio();
+    audioSeven.play();
+});
